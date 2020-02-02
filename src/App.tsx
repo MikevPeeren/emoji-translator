e97230bb2df8ace536dc88e7ec3d1d4a217191a7
MikevPeeren <MikevPeeren@hotmail.com>
2020-02-02 12:48:54 +0100

    Cleaning up React Code

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
+// React
 import React from 'react';
-import logo from './logo.svg';
+
+// CSS
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          <span role="img" aria-label="on-fire">
+            🔥🔥🔥
+          </span>{' '}
+          Translate text to Emoji's !!{' '}
+          <span role="img" aria-label="on-fire">
+            🔥🔥🔥
+          </span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
-}
+};
 
 export default App;
