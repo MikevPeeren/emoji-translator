84abb64be12a3a6a77a6997a312c4b6831ee064b
MikevPeeren <MikevPeeren@hotmail.com>
2020-03-06 15:21:35 +0100

    Updates

---
+++ precache-manifest.585bd5faac471fd518e6894d263c3106.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90b9121effc4adb0ef9034b1e2ed6fe3",
+    "url": "/Emoji-Translator/index.html"
+  },
+  {
+    "revision": "3e1eb55e44ffe2c430ed",
+    "url": "/Emoji-Translator/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "7dbb4cf1807e7ae5dd02",
+    "url": "/Emoji-Translator/static/css/main.4884a308.chunk.css"
+  },
+  {
+    "revision": "3e1eb55e44ffe2c430ed",
+    "url": "/Emoji-Translator/static/js/2.f0b8562b.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/Emoji-Translator/static/js/2.f0b8562b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dbb4cf1807e7ae5dd02",
+    "url": "/Emoji-Translator/static/js/main.ca4fd95e.chunk.js"
+  },
+  {
+    "revision": "10ab3f158030d14931e6",
+    "url": "/Emoji-Translator/static/js/runtime-main.cbba9999.js"
+  }
+]);
