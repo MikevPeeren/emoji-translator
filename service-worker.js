49997f88d373cbd28b5c5019984afde4e2ed5444
MikevPeeren <mikevpeeren@hotmail.com>
2020-03-14 16:36:34 +0000

    Deploying to gh-pages from  - 771b2b63a2e6f0d5245c157afea24d48154c6b72 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Emoji-Translator/precache-manifest.27ffba8cfd2d83141968c6eadf9e9289.js"
+  "/Emoji-Translator/precache-manifest.69e57fdba331299c78e41b480bf5bf3e.js"
 );
 
 self.addEventListener('message', (event) => {
